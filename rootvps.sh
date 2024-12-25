55599511d8ebbfd990b4ff78f62ef4eed522170c
tabraklari <99001325+tabraklari@users.noreply.github.com>
2024-12-25 18:12:45 +0700

    Update rootvps.sh

--- rootvps.sh
+++ rootvps.sh
@@ -3,7 +3,7 @@
 #echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
 
 # Mengunduh file konfigurasi sshd
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/sshd_config
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/tabraklari/rootvps/refs/heads/master/sshd_config
 # Restart layanan SSH
 systemctl restart sshd
 
